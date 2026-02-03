c4e1f05262a04d6e5d8d427dc5ff58f175f01d7d
jzdoot <jacobsamurin@gmail.com>
2026-02-03 12:57:52 -0500

    updated test

--- test/main.py
+++ test/main.py
@@ -1,4 +1,7 @@
-kids = 4
-adults = 2
-output = f"{kids+adults=} total"
-print(output)
+# kids = 4
+# adults = 2
+# output = f"{kids+adults=} total"
+# print(output)
+user_ages = [3, 9, 1, 6, 2]
+
+print(user_ages.index(3))
