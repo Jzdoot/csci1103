605e231a73a09efe83b901e3ca4cdbaeed3099a9
jzdoot <jacobsamurin@gmail.com>
2026-02-05 10:43:08 -0500

    added the new Lab

---
+++ zyBooks/types/listBasics/main.py
@@ -0,0 +1,35 @@
+my_flower1 = input()
+my_flower2 = input()
+my_flower3 = input()
+
+your_flower1 = input()
+your_flower2 = input()
+
+their_flower = input()
+
+# 1. TODO: Define my_list containing my_flower1, my_flower2, and my_flower3
+# in that order
+
+# 2. TODO: Define your_list containing your_flower1 and your_flower2
+# in that order
+
+# 3. TODO: Define our_list by concatenating my_list and your_list
+
+print(our_list)
+
+# 4. TODO: Append their_flower to the end of our_list
+
+print(our_list)
+
+# 5. TODO: Replace my_flower2 in our_list with their_flower
+
+print(our_list)
+
+# 6. TODO: Remove the first occurrence of their_flower from
+# our_list without using index()
+
+print(our_list)
+
+# 7. TODO: Remove the second element of our_list
+
+print(our_list)
